1ddc92d2059339b6756a22d28c86b9f1eefae7d5
Rakshit Kapoor <500067642@stu.upes.ac.in>
2019-01-22 23:18:46 +0530

    2nd Program added

---
+++ Program2.cpp
@@ -0,0 +1,77 @@
+#include<iostream>
+using namespace std;
+	float add(float, float);
+	float subtract(float, float);
+	float multiply(float, float);
+	float divide(float, float);
+
+class Calculator
+{
+	float a, b;
+public:
+	float add(float, float);
+	float subtract(float, float);
+	float multiply(float, float);
+	float divide(float, float);
+};
+int main()
+{
+    char op;
+    float num1, num2;
+
+    cout << "Enter operator either + or - or * or /: ";
+    cin >> op;
+
+    cout << "Enter two operands: ";
+    cin >> num1 >> num2;
+
+    switch(op)
+    {
+        case '+':
+            cout << add(num1, num2);
+            break;
+
+        case '-':
+            cout << subtract(num1, num2);
+            break;
+
+        case '*':
+            cout << multiply(num1, num2);
+            break;
+
+        case '/':
+            cout <<  divide(num1, num2);
+            break;
+
+        default:
+
+            cout << "Error! operator is not correct";
+            break;
+    }
+
+
+}
+float add(float num1, float num2)
+{
+
+    return(num1+num2);
+
+}
+float subtract(float num1, float num2)
+{
+
+    return(num1-num2);
+
+}
+float multiply(float num1,float num2)
+{
+
+    return(num1*num2);
+
+}
+float divide(float num1,float num2)
+{
+
+    return(num1/num2);
+
+}
